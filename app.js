f41ad98721a7a5ee534d7ed1a799546970539f2a
PropWorksTH <propworksth+Github@gmail.com>
2026-02-01 03:34:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const el = document.getElementById('app');
-  if (!el) return;
-
-  fetch('https://script.google.com/macros/s/AKfycbyT2d_zx3w-pQtEhFXcGRCKIqdkEFYCB5jwGrb5nqXjVg6yR910vchMYvKQF2C1R5JE/exec')
-    .then(res => res.json())
-    .then(data => {
-      el.textContent = `${data.app} — ${data.country}`;
-    })
-    .catch(() => {
-      el.textContent = 'Error';
-    });
+  document.getElementById('app').textContent = 'JS LOADED';
 });
