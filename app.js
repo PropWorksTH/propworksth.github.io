5aaa1217b4475a0362d338bf672444d15d1f06a8
PropWorksTH <propworksth+Github@gmail.com>
2026-02-01 03:37:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('app').textContent = 'JS LOADED';
+document.addEventListener('DOMContentLoaded', async () => {
+  const el = document.getElementById('app');
+
+  try {
+    const res = await fetch('https://script.google.com/macros/s/AKfycbyT2d_zx3w-pQtEhFXcGRCKIqdkEFYCB5jwGrb5nqXjVg6yR910vchMYvKQF2C1R5JE/exec');
+    const data = await res.json();
+
+    el.textContent = `${data.app} — ${data.country}`;
+  } catch (e) {
+    el.textContent = 'ERROR';
+  }
 });
