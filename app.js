f5aee99337f8f5d12d0ec3f64fe54eb2adbcb3f6
PropWorksTH <propworksth+Github@gmail.com>
2026-02-01 01:13:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 fetch("https://script.google.com/macros/s/AKfycbyT2d_zx3w-pQtEhFXcGRCKIqdkEFYCB5jwGrb5nqXjVg6yR910vchMYvKQF2C1R5JE/exec?format=json")
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById("app").innerText = data.app;
+  .then(res => res.text())
+  .then(text => {
+    console.log("RAW RESPONSE:", text);
+    document.getElementById("app").innerText = text;
   })
   .catch(err => {
-    document.getElementById("app").innerText = "Error loading data";
+    document.getElementById("app").innerText = "Error";
     console.error(err);
   });
