1504261781935d6d0eff29349794e610bd9d4dc9
PropWorksTH <propworksth+Github@gmail.com>
2026-02-01 01:05:57 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+fetch("https://script.google.com/macros/s/AKfycbyT2d_zx3w-pQtEhFXcGRCKIqdkEFYCB5jwGrb5nqXjVg6yR910vchMYvKQF2C1R5JE/exec?format=json")
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("app").innerText = data.title;
+  })
+  .catch(err => {
+    document.getElementById("app").innerText = "Error loading data";
+    console.error(err);
+  });
