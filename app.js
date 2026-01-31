07c41ce619e45fc5ece0d2f5b100176cef5d183f
PropWorksTH <propworksth+Github@gmail.com>
2026-02-01 04:13:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ document.addEventListener('DOMContentLoaded', async () => {
   const el = document.getElementById('app');
 
   try {
-    const res = await fetch('https://script.google.com/macros/s/AKfycbyT2d_zx3w-pQtEhFXcGRCKIqdkEFYCB5jwGrb5nqXjVg6yR910vchMYvKQF2C1R5JE/exec');
+    const res = await fetch(
+      'https://script.google.com/macros/s/AKfycbyT2d_zx3w-pQtEhFXcGRCKIqdkEFYCB5jwGrb5nqXjVg6yR910vchMYvKQF2C1R5JE/exec?page=items'
+    );
     const data = await res.json();
 
-    el.textContent = `${data.app} — ${data.country}`;
+    el.innerHTML = data.items
+      .map(item => `<div>${item.title} — ${item.subtitle}</div>`)
+      .join('');
   } catch (e) {
     el.textContent = 'ERROR';
   }
