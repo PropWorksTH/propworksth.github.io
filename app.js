aab0bb85ce80ffcd4a1822e02ac389c7d84ecd78
PropWorksTH <propworksth+Github@gmail.com>
2026-02-01 16:02:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log('API ready');
+document.addEventListener('DOMContentLoaded', async () => {
+  const res = await fetch('https://script.google.com/macros/s/AKfycbyT2d_zx3w-pQtEhFXcGRCKIqdkEFYCB5jwGrb5nqXjVg6yR910vchMYvKQF2C1R5JE/exec?page=items');
+  const data = await res.json();
+  console.log(data);
+});
