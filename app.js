a89a4d4641bb46a3e0f668fb2c5f02657374c9dd
PropWorksTH <propworksth+Github@gmail.com>
2026-02-02 10:39:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
-  const res = await fetch('https://script.google.com/macros/s/AKfycbyT2d_zx3w-pQtEhFXcGRCKIqdkEFYCB5jwGrb5nqXjVg6yR910vchMYvKQF2C1R5JE/exec?page=items');
-  const data = await res.json();
-  console.log(data);
+  //const res = await fetch('https://script.google.com/macros/s/AKfycbyT2d_zx3w-pQtEhFXcGRCKIqdkEFYCB5jwGrb5nqXjVg6yR910vchMYvKQF2C1R5JE/exec?page=items');
+  //const data = await res.json();
+  //console.log(data);
 });
